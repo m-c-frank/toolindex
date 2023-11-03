7799a4c131e763a0f9768f105431010d1c00b548
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-11-03 12:36:25 +0100

    added installation of git and gh

--- install.sh
+++ install.sh
@@ -1,46 +1,80 @@
 #!/bin/bash
 
-# Define the install directory
 INSTALL_DIR="$HOME/bin"
-REPO_URL="git@github.com:yourusername/toolindex.git"
+REPO_URL="git@github.com:m-c-frank/toolindex.git"
 REPO_DIR="$HOME/.toolindex"
 
-# Ensure git is installed
+# Function to install git
+install_git() {
+    read -p "git is not installed. Would you like to install it now? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # Install git using the preferred package manager
+        if command -v apt-get &> /dev/null; then
+            sudo apt-get update && sudo apt-get install -y git
+        elif command -v brew &> /dev/null; then
+            brew install git
+        # Add other package managers as needed
+        else
+            echo "Package manager not recognized. Please install git manually."
+            exit 1
+        fi
+    else
+        echo "git is required to continue. Exiting."
+        exit 1
+    fi
+}
+
+# Function to install GitHub CLI
+install_gh() {
+    read -p "GitHub CLI is not installed. Would you like to install it now? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # Install GitHub CLI using the preferred package manager
+        if command -v apt-get &> /dev/null; then
+            sudo apt-get update && sudo apt-get install -y gh
+        elif command -v brew &> /dev/null; then
+            brew install gh
+        # Add other package managers as needed
+        else
+            echo "Package manager not recognized. Please install gh manually."
+            exit 1
+        fi
+    else
+        echo "GitHub CLI is required to continue. Exiting."
+        exit 1
+    fi
+}
+
+# Check if git is installed, if not, offer to install
 if ! command -v git &> /dev/null; then
-    echo "git is not installed. Please install git and try again."
-    exit 1
+    install_git
 fi
 
-# Ensure gh is installed and authenticated
+# Check if gh is installed and authenticated
 if ! command -v gh &> /dev/null; then
-    echo "GitHub CLI is not installed. Please install gh and try again."
-    exit 1
+    install_gh
 fi
 
 if ! gh auth status &> /dev/null; then
-    echo "GitHub CLI is not authenticated. Please login with 'gh auth login'."
-    exit 1
+    gh auth login
 fi
 
-# Clone the repository
+# Clone the repository and install the scripts
 git clone "$REPO_URL" "$REPO_DIR" || {
     echo "Failed to clone the toolindex repository."
     exit 1
 }
 
-# Create bin directory if it doesn't exist and copy scripts
 mkdir -p "$INSTALL_DIR"
 cp "$REPO_DIR/bin/toolindex.sh" "$INSTALL_DIR/toolindex"
 cp "$REPO_DIR/src/fetch_abstracts.sh" "$INSTALL_DIR/"
 cp "$REPO_DIR/src/format_abstracts.sh" "$INSTALL_DIR/"
 
-# Make the scripts executable
 chmod +x "$INSTALL_DIR"/{toolindex,fetch_abstracts.sh,format_abstracts.sh}
 
-# Check if the install directory is in the PATH, add it if not
 if ! echo "$PATH" | grep -q "$INSTALL_DIR"; then
     echo "export PATH=\"$INSTALL_DIR:\$PATH\"" >> "$HOME/.bashrc"
-    echo "Added $INSTALL_DIR to PATH"
 fi
 
-echo "Installation complete. Please restart your terminal or run 'source ~/.bashrc'."
+echo "Installation complete. Please restart your terminal or source your .bashrc file."
