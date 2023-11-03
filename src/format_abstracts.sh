844fd193142216bf7533a90ad322a5743230e684
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-11-03 13:37:19 +0100

    updated format of abstracts

--- src/format_abstracts.sh
+++ src/format_abstracts.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# Takes CSV input and formats it into a human-readable table.
+# Takes CSV input and prints each project title followed by the abstract.
 
-echo "PROJECT,ABSTRACT" | column -t -s,  # Print header
 while IFS=, read -r project abstract; do
-    # Truncate abstract to fit one line and add ellipsis if needed
-    DISPLAY_ABSTRACT=$(echo "$abstract" | cut -c 1-50)
-    if [ "${#abstract}" -gt 50 ]; then
-        DISPLAY_ABSTRACT="${DISPLAY_ABSTRACT}..."
-    fi
-    printf "%-40s | %-50s\n" "$project" "$DISPLAY_ABSTRACT"
-done | column -t -s '|'  # Format as a table
+    echo "PROJECT: $project"
+    echo
+    echo "ABSTRACT:"
+    printf "%b\n" "$abstract"
+    echo
+    echo "________________________________________________________________________________"
+done
